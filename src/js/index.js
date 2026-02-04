884b303a81e2e7aae7d45555c02ab50cdc6f44e7
Rodrigo Costa <rodrigojesusdacosta@gmail.com>
2026-02-04 16:37:48 -0300

    feat: add strenght indicator

--- src/js/index.js
+++ src/js/index.js
@@ -3,6 +3,14 @@ const generateButton = document.querySelector('.generate-button')
 const password = document.querySelector('.password-section__password')
 const lengthBar = document.querySelector('#character-length')
 const lengthNumber = document.querySelector('.character-section__options__length__result__lenght-number')
+const strengthBoxes = document.querySelectorAll('.strength-section__status-section__status-container__box__status')
+const strengthText = document.querySelector('.strength-section__status-section__status-text')
+const tooWeak = document.querySelector('#too-weak')
+const weak = document.querySelector('#weak')
+const medium = document.querySelector('#medium')
+const strong = document.querySelector('#strong')
+
+let strengthCount = 0
 
 const charsets = {
     uppercase: "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
@@ -12,7 +20,7 @@ const charsets = {
 }
 
 
-lengthBar.addEventListener('change', function(){
+lengthBar.addEventListener('change', function () {
 
     lengthNumber.textContent = lengthBar.value
 
@@ -43,21 +51,35 @@ checkboxes.forEach(box => {
 
 
 
-function generatePassword(length, options){
+function generatePassword(length, options) {
     let availableChars = "";
     let generatedPassword = "";
 
 
-    if (options.uppercase) availableChars += charsets.uppercase;
-    if (options.lowercase) availableChars += charsets.lowercase;
-    if (options.numbers) availableChars += charsets.numbers;
-    if (options.symbols) availableChars += charsets.symbols;
+    if (options.uppercase) {
+        availableChars += charsets.uppercase;
+        strengthCount += 1
+    }
+
+    if (options.lowercase) {
+        availableChars += charsets.lowercase;
+        strengthCount += 1
+    }
+
+    if (options.numbers) {
+        availableChars += charsets.numbers;
+        strengthCount += 1
+    }
+    if (options.symbols) {
+        availableChars += charsets.symbols;
+        strengthCount += 1
+    }
 
     if (availableChars === "") return "";
 
     for (let i = 0; i < length; i++) {
         const randomIndex = Math.floor(Math.random() * availableChars.length);
-        
+
         generatedPassword += availableChars.charAt(randomIndex);
     }
 
@@ -68,7 +90,7 @@ function generatePassword(length, options){
 
 generateButton.addEventListener('click', () => {
     const length = lengthBar.value;
-    
+
     const options = {
         uppercase: document.querySelector('#uppercase').checked,
         lowercase: document.querySelector('#lowercase').checked,
@@ -77,12 +99,59 @@ generateButton.addEventListener('click', () => {
     };
 
     const newPassword = generatePassword(length, options);
-    
+
     if (newPassword) {
         password.textContent = newPassword;
         password.classList.remove('password-section__password--inactive')
+
+        console.log(strengthCount)
+
+        strengthBoxes.forEach(box => {
+            box.classList.remove('too-weak')
+            box.classList.remove('weak')
+            box.classList.remove('medium')
+            box.classList.remove('strong')
+        });
+
+        if (strengthCount === 1) {
+            tooWeak.classList.add('too-weak')
+
+            strengthText.textContent = 'TOO WEAK!'
+
+        } else if (strengthCount === 2) {
+
+            tooWeak.classList.add('weak')
+            weak.classList.add('weak')
+
+            strengthText.textContent = 'WEAK'
+
+        } else if (strengthCount === 3) {
+
+            tooWeak.classList.add('medium')
+            weak.classList.add('medium')
+            medium.classList.add('medium')
+
+            strengthText.textContent = 'MEDIUM'
+
+        } else if (strengthCount === 4) {
+
+            tooWeak.classList.add('strong')
+            weak.classList.add('strong')
+            medium.classList.add('strong')
+            strong.classList.add('strong')
+
+            strengthText.textContent = 'STRONG'
+        }
+
+        strengthText.classList.remove('hidden')
+
+        strengthCount = 0
     }
 });
+
+
+
+
 
 
 
